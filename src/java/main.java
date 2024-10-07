c4f6b7d91cbe9e660fb1b0c4c381682ac21fa7cd
Gabriel <gabriel.bc04@hotmail.com>
2024-10-07 12:11:41 +0200

    das auch ignorieren, ich teste nur ein paar sachen auf meine IDE

---
+++ src/java/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Zusätzlicher Code zur Überprüfung der Java-Version
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("Java Version: " + javaVersion);
+        
+        // Einfache Berechnung
+        int sum = 5 + 3;
+        System.out.println("5 + 3 = " + sum);
+    }
+}
